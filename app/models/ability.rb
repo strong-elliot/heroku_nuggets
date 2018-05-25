f63767e6f5c48820a2bacbae819608d810cb174a
Elliot Strong <strong.elliot91@gmail.com>
2018-05-24 17:49:33 -0700

    Removed cancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
-    if user.has_role? :admin
-      can :manage, :all
-    else
-      can :read, :all
-    end
+    #
+    # if user.has_role? :admin
+    #   can :manage, :all
+    # else
+    #   can :read, :all
+    # end
 
 end
     # Define abilities for the passed in user here. For example:
